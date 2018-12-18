f101c2833a8a8138f81021983cfe22d9f7a5d52c
GameBelial <243387971@qq.com>
2018-12-18 22:24:14 +0800

    🚧 代码施工当中...

---
+++ LightningPT.BitTorrentGenerate/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BencodeNET.Parsing;
+using BencodeNET.Torrents;
+
+namespace LightningPT.BitTorrentGenerate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = new BencodeParser();
+            var torrent =
+                parser.Parse<Torrent>(
+                    "/Users/zony/Downloads/[GZtown].IMDb.Top.250.BluRay.1080p.x265.10bit.MNHD-FRDS.torrent");
+            var t2 = new Torrent();
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
